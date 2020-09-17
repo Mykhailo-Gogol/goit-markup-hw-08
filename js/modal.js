bca0c63b250555cb457694d5c0a27136f514510c
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2020-09-17 17:29:15 +0300

    Додав стилі для переключення кнопки меню, Додав JS

--- js/modal.js
+++ js/modal.js
@@ -2,3 +2,8 @@ function myFunction() {
   let element = document.getElementById("myId");
   element.classList.toggle("is-hidden");
 }
+
+function myFunctionMenu() {
+  let element = document.getElementById("myIdMenu");
+  element.classList.toggle("is-open");
+}
